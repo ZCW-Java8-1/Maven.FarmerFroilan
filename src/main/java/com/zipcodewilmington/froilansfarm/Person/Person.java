c6a8cabc618d283ff1d0fa7495f7e972531a0177
Tenae <tenae@Zipcoders-MacBook-Pro-107.local>
2022-07-21 17:18:57 -0400

    just keeping up tp date

--- src/main/java/com/zipcodewilmington/froilansfarm/Person/Person.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Person/Person.java
@@ -3,7 +3,11 @@ package com.zipcodewilmington.froilansfarm.Person;
 import com.zipcodewilmington.froilansfarm.GenericUtils.Eater;
 import com.zipcodewilmington.froilansfarm.GenericUtils.NoiseMaker;
 
-public interface Person <Eater> extends NoiseMaker {
+public interface Person <Eater> extends NoiseMaker{
     String makeNoise();
+
+    public Eater eat();
+
+
 
 }
