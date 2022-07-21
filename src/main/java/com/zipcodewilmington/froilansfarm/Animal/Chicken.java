6a14958fa0e7d9f9f433b5996d65b0b94443238c
LloydJr <98184266+LloydJr@users.noreply.github.com>
2022-07-21 16:52:45 -0400

    Update Code

--- src/main/java/com/zipcodewilmington/froilansfarm/Animal/Chicken.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Animal/Chicken.java
@@ -1,11 +1,22 @@
 package com.zipcodewilmington.froilansfarm.Animal;
 
-public class Chicken {
+import com.zipcodewilmington.froilansfarm.GenericUtils.Eater;
+import com.zipcodewilmington.froilansfarm.GenericUtils.NoiseMaker;
+
+public class Chicken <ChickenType> extends Animal {
 
     public Chicken(Integer id, Integer age, Boolean hasBeenFed){
-        super();
+        super(id, age, hasBeenFed);
     }
     public void yield() {
 
+    }
+
+    public void eat() {
+
+    }
+
+    public String makeNoise() {
+        return "Peep!";
     }
 }
--- src/main/java/com/zipcodewilmington/froilansfarm/Animal/Horse.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Animal/Horse.java
@@ -3,9 +3,17 @@ package com.zipcodewilmington.froilansfarm.Animal;
 import com.zipcodewilmington.froilansfarm.GenericUtils.NoiseMaker;
 import com.zipcodewilmington.froilansfarm.GenericUtils.Rider;
 
-public class Horse <HorseType extends Rider>{
+public class Horse <HorseType extends Rider> extends Animal{
 
     public Horse(Integer id, Integer age, Boolean hasBeenFed){
-        super();
+        super(id, age, hasBeenFed);
+    }
+
+    public void eat() {
+
+    }
+
+    public String makeNoise() {
+        return "Neigh!";
     }
 }
