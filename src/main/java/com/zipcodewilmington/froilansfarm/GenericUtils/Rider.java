6d4bad0d699b138b5fbccaefb596ef38a1ecf72b
LloydJr <98184266+LloydJr@users.noreply.github.com>
2022-07-22 16:04:50 -0400

    Refactored Animal Files

--- src/main/java/com/zipcodewilmington/froilansfarm/GenericUtils/Rider.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/GenericUtils/Rider.java
@@ -1,6 +1,5 @@
 package com.zipcodewilmington.froilansfarm.GenericUtils;
 
-import com.sun.org.apache.xpath.internal.operations.Bool;
 import com.zipcodewilmington.froilansfarm.Person.Person;
 
 public interface Rider <RiderType extends Person> {
--- src/test/java/com/zipcodewilmington/froilansfarm/Animal/AnimalTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/Animal/AnimalTest.java
@@ -3,5 +3,5 @@ package com.zipcodewilmington.froilansfarm.Animal;
 import static org.junit.Assert.*;
 
 public class AnimalTest {
-
+//No test needed :^)
 }
