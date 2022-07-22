00620e78f612dde0541726565823b5660dab29a6
octavia <Ogomes21@gmail.com>
2022-07-22 14:42:40 -0400

    Added corn & tomato.

---
+++ src/main/java/com/zipcodewilmington/froilansfarm/crops/Corn.java
@@ -0,0 +1,16 @@
+package com.zipcodewilmington.froilansfarm.crops;
+
+import com.zipcodewilmington.froilansfarm.Produce;
+
+public class Corn extends Produce {
+
+    public Corn() {
+        super("corn");
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
+
--- src/main/java/com/zipcodewilmington/froilansfarm/crops/TomatoPlant.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/crops/TomatoPlant.java
@@ -1,4 +1,5 @@
 package com.zipcodewilmington.froilansfarm.crops;
 
 public class TomatoPlant extends Crop {
+
 }
