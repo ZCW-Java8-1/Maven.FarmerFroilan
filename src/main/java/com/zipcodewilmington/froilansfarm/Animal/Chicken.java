9abe31cc224d2679b924d78d2304397d18108c74
LloydJr <98184266+LloydJr@users.noreply.github.com>
2022-07-22 10:21:40 -0400

    Refactored Classes

--- src/main/java/com/zipcodewilmington/froilansfarm/Animal/Chicken.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Animal/Chicken.java
@@ -2,22 +2,17 @@ package com.zipcodewilmington.froilansfarm.Animal;
 
 import com.zipcodewilmington.froilansfarm.GenericUtils.Eater;
 import com.zipcodewilmington.froilansfarm.GenericUtils.NoiseMaker;
+import com.zipcodewilmington.froilansfarm.Produce.Crop;
 import com.zipcodewilmington.froilansfarm.Produce.Produce;
 
-public class Chicken extends Animal implements Produce {
+public class Chicken <Animal> implements Produce {
 
-    public Chicken(Integer id, Integer age, Boolean hasBeenFed){
-        super(id, age, hasBeenFed);
-    }
-    public void yield() {
 
+    public Boolean fertilize() {
+        return null;
     }
 
-    public void eat() {
-
-    }
-
-    public String makeNoise() {
-        return "Peep!";
+    public Crop plant() {
+        return null;
     }
 }
