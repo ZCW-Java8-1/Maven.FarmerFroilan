b2c04c25ce5cc11c63e9fde5271f49c8b6655b60
LloydJr <98184266+LloydJr@users.noreply.github.com>
2022-07-23 15:40:53 -0400

    Act

--- src/test/java/com/zipcodewilmington/froilansfarm/Animal/HorseTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/Animal/HorseTest.java
@@ -12,12 +12,14 @@ public class HorseTest {
 
     @Test
     public void isAnAnimalTest(){
+        //Arrange
         Horse horse = new Horse();
+        //Assert
         Assert.assertTrue(horse instanceof Animal);
     }
     @Test
     public void mountedTest() {
-        //Given
+        //Arrange
         Horse horse = new Horse();
         Farmer farmer = new Farmer();
         //When
@@ -26,17 +28,20 @@ public class HorseTest {
 
     @Test
     public void dismountedTest() {
-        //Given
+        //Arrange
         Horse horse = new Horse();
         Farmer farmer = new Farmer();
-        //When
+        //Assert
         Assert.assertEquals(true, farmer.dismount(horse));
     }
 
     @Test
     public void makeNoiseTest(){
+        //Arrange
         Horse horse = new Horse();
+        //Act
         String noise = "Neigh!";
+        //Assert
         Assert.assertEquals(noise, horse.makeNoise());
 
     }
